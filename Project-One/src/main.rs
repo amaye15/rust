4adf916e4d0c24857492997c3f2abd74d7a71d39
amaye15 <70704207+amaye15@users.noreply.github.com>
2023-08-16 17:09:42 +0000

    mutable variables

--- Project-One/src/main.rs
+++ Project-One/src/main.rs
@@ -4,7 +4,12 @@ fn main() {
     let str_var: &str = "Hi";
     let int_var: i32 = 2;
     let float_var: f64 = 0.2;
+    let mut mut_var = "Hello";
     println!("{}", str_var);
     println!("{}", int_var);
     println!("{}", float_var);
+    println!("{}", mut_var);
+
+    mut_var = "Good Morning";
+    println!("{}", mut_var);
 }
